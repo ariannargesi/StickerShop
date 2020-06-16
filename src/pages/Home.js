304936b6692c8fdfa8b1724101b1da3111f98a03
arian <ariannargesi@gmail.com>
2020-06-16 21:59:02 +0430

    category page added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import Header from '../layouts/Header/Header'
 import Footer from '../layouts/Footer/Footer'
 import Carousel from '../components/Carousel/Carousel'
 import Title from '../components/Title/Title'
+import Pagination from '../layouts/Pagination/Pagination'
 import ProductsRow from '../components/ProductsRow/ProductsRow'
 
 export default () => (
@@ -10,7 +11,7 @@ export default () => (
         <Header/>
         {/*<Carousel/>*/}
         <Title title="Newest Products"/>
-        {/*<ProductsRow num="4"/>*/}
-        <Footer/>
+        <ProductsRow num="4"/>
+        <Pagination/>
     </div>
 )
---
+++ src/pages/category/Category.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from "../../layouts/Header/Header";
+import Footer from "../../layouts/Footer/Footer";
+const Category = () => {
+    return (
+        <>
+            <Header/>
+            some content
+            <Footer/>
+        </>
+    )
+}
+export default Category
