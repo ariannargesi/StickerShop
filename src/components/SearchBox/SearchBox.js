8057c5488f84426537f949c27c4ac7d25c2c5cfd
arian <ariannargesi@gmail.com>
2020-06-20 00:50:34 +0430

    Loading component added

--- src/components/SearchBox/SearchBox.js
+++ src/components/SearchBox/SearchBox.js
@@ -1,21 +1,37 @@
-import React,{useState,useRef} from 'react'
+import React,{useState,useEffect} from 'react'
+import axios from 'axios'
 import './SearchBox.css'
+import Loading from "../Loading/Loading";
 
 const SearchBox = () => {
     const [isOpen, setOpen] = useState(false)
     const [input, setInput] = useState("")
-    const ref = useRef(null)
+    const [data, setData] = useState(null)
+
+    useEffect(()=>{
+        axios.get(`https://flerbo.herokuapp.com/api/s/search?term=${input}`)
+            .then(result => {
+                if(result.data.status == 200) {
+                    result.data.products.length == 0 ? setData("Nothing found") : setData(result.data.products)
+                    console.log(data)
+                }
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    })
+
     function handelOpen(){
         if(isOpen == false )
             setOpen(true)
     }
-
     function handelClose(){
         setOpen(false)
     }
     function handelInputChange(event){
         setInput(event.target.value)
      }
+
     return (
         <div className="search-wrapper" onClick={handelOpen}>
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
@@ -38,12 +54,18 @@ const SearchBox = () => {
                     <form action="">
                         <input value={input} onChange={handelInputChange} type="text" placeholder="Name or Category" className="input-search-box"/>
                         <button disabled={input.length == 0 ? true : false}  type="submit" className="search-search-box" className="submit-search-box" >search</button>
-
                     </form>
+                    {
+                        input &&
+                            <div>
+
+                            </div>
+                    }
                 </div>
                 :
                 <></>
             }
+
         </div>
     )
 }
