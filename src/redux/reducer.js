d3a43579af3cd940e041083396079daa02c30f22
arian <ariannargesi@gmail.com>
2020-06-26 06:29:07 +0430

    add to card fetuhore aadded

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,22 +1,37 @@
 import { ADD_TO_CARD, REMOVE_FROM_CARD } from './types'
 const initalStae = []
 const reducer = (state = initalStae, action) => {
-
     if(action.type === ADD_TO_CARD){
-        let newState = state 
-        const payload = action.payload 
+      const newState = state
+      const payload = action.payload
+      function isNewItem (product){
+        const result = newState.find(item => {
+	         return item.product.title === payload.title
+         })
+         if(result)
+             return false
+         else
+             return true
+      }
+      const newItem = isNewItem(payload)
+      if(newItem){
+        newState.push({ product: payload, quantity: 1 })
+        console.log("im returning new state ")
+        return newState
+      }
+
+      else {
         newState.map(item => {
-            if( item.product == payload ) {
-                item.quantity +1 
-                return item 
-            }
-            return {
-                product,
-                quantity: 1
-            }
-        })
-    } 
-    return state 
+ 	        if(item.product.title === payload.title)
+    	     item.quantity++
+      })
+      console.log("dont run")
+      }
+
+    }
+
+    return state
 }
 
-export default reducer 
+
+export default reducer
