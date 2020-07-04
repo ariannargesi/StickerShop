7e4ee194262f769f08b8b9a90da3c2b5b52e4e33
arian <ariannargesi@gmail.com>
2020-07-04 17:33:23 +0430

    3k remote job is the thing that i want

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import './style.css'
+import './style.scss'
 const root = document.getElementById('root')
 ReactDOM.render(<App/>, root)
