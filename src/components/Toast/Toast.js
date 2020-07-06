52e00b54aaacda44cd58f591a4ea58ab6a192d0d
arian <ariannargesi@gmail.com>
2020-07-06 12:45:27 +0430

    transition Added

--- src/components/Toast/Toast.js
+++ src/components/Toast/Toast.js
@@ -1,19 +1,30 @@
 import React, {useEffect} from 'react' 
 import { connect } from 'react-redux'
 import {showToast} from '../../redux/actions'
+import { useTransition, animated } from 'react-spring'
 import './Toast.scss'
 const Toast = (props) => {   
     const toastTxt = props.toast 
+    
     useEffect(() => {
         if(toastTxt)
         setTimeout(() => {
                 props.showToast("")
-        }, 3000)
+        }, 1000)
     })
 
+    const transitions = useTransition(toastTxt ? true : false, null, {
+        from: {  opacity: 0 },
+        enter: { opacity: 1 },
+        leave: { opacity: 0 },
+    })
+    const getTransitions = transitions.map(({ item, key, props }) =>
+             item && <animated.div key={key} style={props}><div className="toast">  {toastTxt} </div>  </animated.div>
+    )
+    
     return (
         <div className={"toast-container"}>
-            
+           {getTransitions}
         </div>
     )
 }
