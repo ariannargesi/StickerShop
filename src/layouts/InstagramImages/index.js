68669cf09a879ba376b6ae72729e3948741d4c30
arian <ariannargesi@gmail.com>
2020-07-11 12:54:54 +0430

    text-overflow: ellipsis;

--- src/layouts/InstagramImages/index.js
+++ src/layouts/InstagramImages/index.js
@@ -1,21 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react'
+import axios from 'axios'
 import './InstagramImages.scss'
-import Title from "../../components/Title/Title";
-import wave2 from '../../assets/wave2.svg'
-import {findRenderedDOMComponentWithTag} from "react-dom/test-utils";
 
-const Index = () => {
+const InstagramImages = () => {
 
+    const [list, setList] = useState(null)
+    useEffect(() => {
+       axios({
+           url: "https://www.instagram.com/ariannargesi/?__a=1",
+           method: "get",
+           headers: {'Content-Type': 'application/json'}
+       }).then( result => {
+           console.log(result)
+       } ).catch(err => console.log(err))
+    }, [list])
 
     return (
-        <div className="instagram-images">
-            <Title
-                content={"Fallow us on instagram"}
-                size={"small"}
-            />
-        </div>
+        <div></div>
     )
 }
 
+export default InstagramImages
 
-export default Index
+
