d1d86838c7d4e7b26a41cda543e226036ac83d56
arian <ariannargesi@gmail.com>
2020-07-12 13:46:34 +0430

    wizard tab bug fixed

--- src/components/WizardTab/index.js
+++ src/components/WizardTab/index.js
@@ -1,26 +1,26 @@
 import React, { useState } from 'react'
 import './WizardTab.scss'
 
-const WizardTab = ({steps}) => {
+const Index = (props) => {
     const [currentTab, setCurrentTab] = useState(0)
     const nextClickHandler = () => {
-        if(currentTab < steps.length)
+        if(currentTab < props.children.length -1)
             setCurrentTab(currentTab+1)
     }
     const prevClickHandler = () => {
         if(currentTab > 0)
             setCurrentTab(currentTab-1)
-        setCurrentTab(currentTab-1)
+
     }
 
     return (
-        <div>
+        <div className="wizard-tab">
           <div>
-              { steps[currentTab] }
+              { props.children[currentTab] }
               <button onClick={nextClickHandler} >Next</button>
               <button onClick={prevClickHandler}>Prev</button>
           </div>
         </div>
     )
 }
-export default WizardTab
+export default Index
--- src/layouts/Checkout/index.js
+++ src/layouts/Checkout/index.js
@@ -1,11 +1,16 @@
 import React from 'react'
+import WizardTab from '../../components/WizardTab'
 import './Checkout.scss'
 
 
 const Checkout = () => {
     return (
         <div>
-
+            <WizardTab>
+                <h1>One</h1>
+                <h1>Two</h1>
+                <h1>Three</h1>
+            </WizardTab>
         </div>
     )
 }
