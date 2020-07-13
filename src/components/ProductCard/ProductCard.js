741be866517c5c1844a7c04cb6afa2405a30d171
arian <ariannargesi@gmail.com>
2020-07-13 19:11:16 +0430

    styles and icon changed

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -25,13 +25,7 @@ const ProductCard =  props => {
                         </div>
                     </NavLink>
                     <button className="add-to-card-small" onClick={()=> { props.addToCard(product); props.showToast("Sticker Added To You Cart")  }}>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-                             stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
-                             className="feather feather-shopping-bag">
-                            <path d="M6 2L3 6v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V6l-3-4z"></path>
-                            <line x1="3" y1="6" x2="21" y2="6"></line>
-                            <path d="M16 10a4 4 0 0 1-8 0"></path>
-                        </svg>
+                        <span className="material-icons">enhanced_encryption</span>
                     </button>
                 </div>
             </div>
