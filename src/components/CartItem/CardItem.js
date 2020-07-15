37d1d0d769864c9b94da87d227c7f1604a50b190
arian <ariannargesi@gmail.com>
2020-07-15 11:11:50 +0430

    time to make it responsive

--- src/components/CartItem/CardItem.js
+++ src/components/CartItem/CardItem.js
@@ -5,60 +5,60 @@ import trashIcon from '../../assets/icons/trash.svg'
 import './CardItem.scss'
 
 const CardItem = props => {
-  const { product } = props.data
-  const { quantity } = props.data
+    const { product } = props.data
+    const { quantity } = props.data
 
-  const getQuantity = () => {
-    const result = props.card.map(item => {
-      if(item.product.title == product.title) {
-        return item
-      }
-    })
-    return result[0].quantity
-  }
-  const removeClickHandler = () => {
-    props.removeFromCard(product)
-  }
-  const increaseQuantityHandler = () => {
-    props.increaseProductQantity(product)
-  }
-  const decreaseQuantityHandler = () => {
-    props.decreaseProductQantity(product)
-  }
-  return (
-      <div className={"card-item"}>
-        <div className="card-thumb"><img src={product.images[0]} alt=""/> </div>
-        <div className="card-details">
-          <button className="remove-item" onClick={removeClickHandler}>
-            <span className="material-icons">delete_outline</span>
-          </button>
-          <h5> {product.title} </h5>
-          <div className="size-and-type">
-            <span>type: {product.types[0]}</span>
-            <span>size: {product.sizes[0]} </span>
-          </div>
-          <div className="card-item-footer">
-            <div>
-              <span className="price"> $ { product.price } </span>
+    const getQuantity = () => {
+        const result = props.card.map(item => {
+            if(item.product.title == product.title) {
+                return item
+            }
+        })
+        return result[0].quantity
+    }
+    const removeClickHandler = () => {
+        props.removeFromCard(product)
+    }
+    const increaseQuantityHandler = () => {
+        props.increaseProductQantity(product)
+    }
+    const decreaseQuantityHandler = () => {
+        props.decreaseProductQantity(product)
+    }
+    return (
+        <div className={"card-item"}>
+            <div className="card-thumb"><img src={product.images[0]} alt=""/> </div>
+            <div className="card-details">
+                <button className="remove-item" onClick={removeClickHandler}>
+                    <span className="material-icons">delete_outline</span>
+                </button>
+                <h5> {product.title} </h5>
+                <div className="size-and-type">
+                    <span>type: {product.types[0]}</span>
+                    <span>size: {product.sizes[0]} </span>
+                </div>
+                <div className="card-item-footer">
+                    <div>
+                        <span className="price"> $ { product.price } </span>
+                    </div>
+                    <div style={{ border: "1px solid #eee", borderRadius: "5px" }}>
+                        <button onClick={increaseQuantityHandler}>
+                            <span className="material-icons">add</span>
+                        </button>
+                        <span style={{marginTop: "2px"}}> {getQuantity()} </span>
+                        <button onClick={decreaseQuantityHandler}>
+                            <span className="material-icons">remove</span>
+                        </button>
+                    </div>
+                </div>
             </div>
-            <div style={{ border: "1px solid #eee", borderRadius: "5px" }}>
-              <button onClick={increaseQuantityHandler}>
-                <span className="material-icons">add</span>
-              </button>
-              <span style={{marginTop: "2px"}}> {getQuantity()} </span>
-              <button onClick={decreaseQuantityHandler}>
-                <span className="material-icons">remove</span>
-              </button>
-            </div>
-          </div>
         </div>
-      </div>
-  )
+    )
 }
 const mapStateToProps = ({card}) => {
-  return {
-    card
-  }
+    return {
+        card
+    }
 
 }
 export default connect(mapStateToProps, {increaseProductQantity, removeFromCard, decreaseProductQantity})(CardItem)
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import './responsive.scss'
 import './style.scss'
 const root = document.getElementById('root')
 ReactDOM.render(<App/>, root)
