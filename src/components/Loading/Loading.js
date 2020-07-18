1116053e2b5b33974f202d5624279de1a013ba06
arian <ariannargesi@gmail.com>
2020-07-18 18:25:29 +0430

    Loading component work like pitch

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,9 +1,13 @@
 import React from 'react'
-import './Loading.sass'
-export default () => {
+import './Loading.scss'
+
+const Loading = (props) => {
     return (
-        <div>
-            Loading...
+        <div className="loading" style={{width: props.width, height: props.height}} >
+            <div className="loading-dot" ></div>
+            <div style={{animationDelay: "200ms"}} className="loading-dot" ></div>
+            <div className="loading-dot" ></div>
         </div>
     )
 }
+export default Loading
