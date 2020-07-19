067ef13a819b78c96144011ad966f498f5a83246
arian <ariannargesi@gmail.com>
2020-07-19 20:41:17 +0430

    styles edited

--- src/layouts/SignIn/index.js
+++ src/layouts/SignIn/index.js
@@ -5,17 +5,33 @@ import React from "react";
 import validator from "validator";
 import Button from "../../components/Button";
 
+const styles = {
+    width: "400px",
+    border: "1px solid #d4d4d4",
+    padding: "1rem",
+    borderRadius: "10px",
+    background: "white",
+    boxShadow: "0px 0px 5px 5px #eee"
+}
+const loginStyles = {
+    display: "flex",
+}
 const SignIn = () => {
     return (
         <Modal title={"Sign in"}>
-            <div style={{width: "350px"}}>
+            <div style={styles}>
                 <h2>Create a new account</h2>
                 <form action="" className="log-sign-form">
-                    <Input type="text" placeholder="username" message={hints.username} />
-                    <Input type="email" placeholder={"Email Address"} message={hints.email} />
-                    <Input type="password" placeholder= "password" message={hints.password}/>
+                    <Input type="text" placeholder="Username" message={hints.username} />
+                    <Input type="email" placeholder={"Email"} message={hints.email} />
+                    <Input type="password" placeholder= "Password" message={hints.password}/>
                     <Button  size="medium" type={"success"}>Sign in</Button>
-                    <Login/>
+                    <div style={loginStyles}>
+                        <span style={{color: "gray"}}> Already Have an account ? </span>
+                        <div>
+                            <Login/>
+                        </div>
+                    </div>
                 </form>
             </div>
 
