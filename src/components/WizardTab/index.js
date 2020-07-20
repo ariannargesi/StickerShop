f23e7349ec304a8fc64aae146d7276e50c4b9423
arian <ariannargesi@gmail.com>
2020-07-20 17:54:42 +0430

    Button added

--- src/components/WizardTab/index.js
+++ src/components/WizardTab/index.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react'
+import Button from "../Button"
 import './WizardTab.scss'
 
-const Index = (props) => {
+const Index = ({children, styles}) => {
     const [currentTab, setCurrentTab] = useState(0)
     const nextClickHandler = () => {
-        if(currentTab < props.children.length -1)
+        if(currentTab < children.length -1)
             setCurrentTab(currentTab+1)
     }
     const prevClickHandler = () => {
@@ -16,9 +17,11 @@ const Index = (props) => {
     return (
         <div className="wizard-tab">
           <div>
-              { props.children[currentTab] }
-              <button onClick={nextClickHandler} >Next</button>
-              <button onClick={prevClickHandler}>Prev</button>
+              { children[currentTab] }
+              <div className="wizard-navigation">
+                  <Button type="success"  size="medium" width={"200px"} onClick={nextClickHandler} >Next</Button>
+                  <Button type="success"  size="medium" width={"200px"} onClick={prevClickHandler}>Prev</Button>
+              </div>
           </div>
         </div>
     )
