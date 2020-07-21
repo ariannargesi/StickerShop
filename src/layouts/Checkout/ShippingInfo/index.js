b5fca51a4f25a566a84ccc55ce3d85bd6a9ffc3d
arian <ariannargesi@gmail.com>
2020-07-21 19:36:08 +0430

    change the wrapper width

--- src/layouts/Checkout/ShippingInfo/index.js
+++ src/layouts/Checkout/ShippingInfo/index.js
@@ -8,35 +8,35 @@ const ShippingInfo = () => {
         <div className="shipping-info">
             <div className="order-info">
                 <div className="order-info-column">
-                    <div style={{width: "25%"}}>
-                        <Input name={"Name"}/>
+                    <div style={{width: "24%"}}>
+                        <Input name={"First Name"} />
                     </div>
-                    <div style={{width: "25%"}}>
-                        <Input name={"Name"}/>
+                    <div style={{width: "24%"}}>
+                        <Input name={"Last Name"}/>
                     </div>
-                    <div style={{width: "45%"}}>
-                        <Input name={"Name"}/>
+                    <div style={{width: "44%"}}>
+                        <Input name={"Email Address"}/>
                     </div>
                 </div>
                 <div className="order-info-column">
-                    <div style={{width: "25%"}}>
+                    <div style={{width: "24%"}}>
                         <Input type={"text"} name={"Zip Code"}/>
                     </div>
-                    <div style={{width: "25%"}}>
-                        <Input type={"text"} name={"Zip Code"}/>
+                    <div style={{width: "24%"}}>
+                        <Input type={"text"} name={"Phone Number"}/>
                     </div>
-                    <div style={{width: "43%", marginRight: "8px"}}>
-                        <Dropdown items={statesList}  title={"State"} current={statesList[0]} />
+                    <div style={{width: "44%", marginRight: "0px"}}>
+                        <Dropdown items={statesList}  title={"Location"} current={statesList[0]} />
                     </div>
                 </div>
                 <div className="order-info-column">
                     <div className={"full-width"}>
-                        <Input type={"text"} name={"Address"} />
+                        <Input type={"text"} name={"Full Address"} />
                     </div>
                 </div>
                 <div className="order-info-column">
                     <div className={"full-width"}>
-                        <Input type={"text"} name={"Address"} />
+                        <Input type={"text"} name={"Descriptions (Optional)"} placeholder={"In Case of Not being at Home, Delivery the Order to My Neighbor Penny"} />
                     </div>
                 </div>
             </div>
