d22a3e85f14e0fdfae20032f00d58f1567dc2c29
arian <ariannargesi@gmail.com>
2020-07-22 12:19:28 +0430

    set class name for items

--- src/components/Select/index.js
+++ src/components/Select/index.js
@@ -5,13 +5,13 @@ const Select = ({children, direction}) => {
     const [currentItemIndex, setCurrentItemIndex] = useState(0)
     const styles = {
         display: "flex",
-        flexDirection: direction || "row"
+        flexDirection: direction || "row",
     }
     return (
         <div style={styles} >
             {
                 children.map((item, index) => {
-                    return <div className={ (index == currentItemIndex ? "active" : "") } onClick={ () => setCurrentItemIndex(index) }>{item}</div>
+                    return <div className={ (index == currentItemIndex ? "active select-item" : "select-item") } onClick={ () => setCurrentItemIndex(index) }>{item}</div>
                 })
             }
         </div>
