015d46dbe9334dafe98812447a093497cde351ca
arian <ariannargesi@gmail.com>
2020-07-23 13:18:33 +0430

    transition added

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -4,20 +4,27 @@ import useClickOutside from '../../hooks/useClickOutside'
 import './Modal.scss'
 export default  (props) => {
     const [state, setState] = useToggle(false)
+    const [anim, setAnim] = useState("modal")
     const divRef = useRef(null)
     const clickHandler = () => {
-        setState(!state)
+        setState(true)
+            setTimeout(() => {
+               setAnim("modal modal-open")
+           },1)
     }
+
     const clickOutsideHandler = () => {
-        setState(false)
-    }
+        setAnim("modal")
+        setTimeout(() => {
+            setState(false)
+        },300)    }
     useClickOutside(divRef, clickOutsideHandler)
     return (
         <div className="modal-wrapper">
             <span className="modal-title" onClick={clickHandler}>{props.title}</span>
             { state &&
-            <div className="modal">
-                <span onClick={clickHandler} className="close-modal material-icons">close</span>
+            <div className={anim}>
+                <span className="close-modal material-icons">close</span>
                 <div className="modal-content" ref={divRef}>
                     {
                         props.children
