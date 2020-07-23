4e6736a0a68aa0fa885ee4d24962729f185d3137
arian <ariannargesi@gmail.com>
2020-07-23 12:46:44 +0430

    styles chnaged

--- src/components/MenuItem/MenuItem.js
+++ src/components/MenuItem/MenuItem.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {NavLink} from "react-router-dom";
-import './MenuItem.sass'
+import './MenuItem.scss'
 export default (props) => {
     let className = "menu-column"
     if(props.mode == "bold")
