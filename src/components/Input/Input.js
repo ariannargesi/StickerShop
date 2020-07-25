af62d5800367490ec0b02ad4e472932683d1df5b
arian <ariannargesi@gmail.com>
2020-07-25 21:31:55 +0430

    seconde parameter added to useEffect

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -10,7 +10,7 @@ export default (props) => {
              setMessage(props.message)
         else if(typeof props.message == "function")
              setMessage(props.message(input))
-    })
+    }, [input])
     const changeHandler = (event) => {
         setInput(event.target.value)
     }
