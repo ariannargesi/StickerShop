cc27df84df32e5d0959278da6fcc83abaa149897
arian <ariannargesi@gmail.com>
2020-07-25 20:58:02 +0430

    remove swiper import

--- src/layouts/Slider/index.js
+++ src/layouts/Slider/index.js
@@ -1,6 +1,4 @@
-import { Swiper, SwiperSlide } from 'swiper/react';
 import React from 'react'
-import 'swiper/swiper.scss'
 import ProductsRow from "../../components/ProductsRow/ProductsRow";
 
 const Slider = () => {
