89afcb71ab6ef1b3716c067e6c581338bc077fa9
Arian Nargesi <ariannargesi@gmail.com>
2020-09-24 15:28:04 +0330

    refactor

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,23 +1,25 @@
-import React from 'react'
-import './Button.scss'
+import React from "react";
+import "./Button.scss";
 
-const Button = ({size, type, children, onClick, style, width, disable}) => {
-    const sizes = {
-        large: "24x",
-        medium: "18px",
-        small: "18px"
-    }
+const Button = (props) => {
+  const sizes = {
+    large: "24x",
+    medium: "18px",
+    small: "18px",
+  }
+  //Dynamically access object property using Bracket notation
+  const fontSize = sizes[props.size];
+  const className = ["button", props.type].join(" ");
 
-    const fontSize = sizes[size]
-    return (
-        <button
-        className={["button", type ].join(' ')}
-            style={{fontSize,width: width || "100%"  }}
-            onClick={onClick}
-            disabled={disable}
-        >
-            {children}
-        </button>
-    )
-}
-export default Button
+  const style = {
+    fontSize,
+    width: props.width || "100%",
+  };
+
+  return (
+    <button className={className} style={style} {...props}>
+      {props.children}
+    </button>
+  );
+};
+export default Button;
